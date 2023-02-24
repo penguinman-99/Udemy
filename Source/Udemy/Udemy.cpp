7ab3f0cde1d21ba174f3ac66ee50b299f742d876
KKH <lateson6@gmail.com>
2023-02-24 19:00:20 +0900

    add Udemy Project

---
+++ Source/Udemy/Udemy.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Udemy.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Udemy, "Udemy" );
+ 
