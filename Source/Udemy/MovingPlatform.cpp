22f1940df241ace97e847402e3acfb2321a6e7ea
penguinman-99 <61744551+penguinman-99@users.noreply.github.com>
2023-03-03 20:57:39 +0900

    learn to platform trigger

--- Source/Udemy/MovingPlatform.cpp
+++ Source/Udemy/MovingPlatform.cpp
@@ -25,23 +25,36 @@ void AMovingPlatform::BeginPlay()
 void AMovingPlatform::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-	
-	//True: 서버, 
-	if (HasAuthority()) {
-		FVector Location = GetActorLocation();
-		float JourneyLength = (GlobalTargetLocation - GlobalStartLocation).Size();
-		float JourneyTravelled = (Location - GlobalStartLocation).Size();
-		if (JourneyTravelled >= JourneyLength) {
-			FVector t = GlobalTargetLocation;
-			GlobalTargetLocation = GlobalStartLocation;
-			GlobalStartLocation = t;
+	if (ActiveTriggers > 0) {
+		if (HasAuthority()) {
+			FVector Location = GetActorLocation();
+			float JourneyLength = (GlobalTargetLocation - GlobalStartLocation).Size();
+			float JourneyTravelled = (Location - GlobalStartLocation).Size();
+			if (JourneyTravelled >= JourneyLength) {
+				FVector t = GlobalTargetLocation;
+				GlobalTargetLocation = GlobalStartLocation;
+				GlobalStartLocation = t;
+			}
+			FVector tmp = (GlobalTargetLocation - GlobalStartLocation).GetSafeNormal();
+			Location += tmp * speed * DeltaTime;
+
+			SetActorLocation(Location);
 		}
-		FVector tmp = (GlobalTargetLocation - GlobalStartLocation).GetSafeNormal();
-		Location += tmp * speed * DeltaTime;
+		else {
 
-		SetActorLocation(Location);
+		}
 	}
-	else {
+	//True: 서버, 
+	
+}
 
-	}
+void AMovingPlatform::AddActiveTrigger()
+{
+	ActiveTriggers +=1;
+}
+
+void AMovingPlatform::RemoveActiveTrigger()
+{
+	if(ActiveTriggers>0)
+		ActiveTriggers -=1;
 }
