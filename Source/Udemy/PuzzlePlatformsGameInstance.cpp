d91c2a0d7501557ed283b31ca1d0f02ab6775577
penguinman-99 <61744551+penguinman-99@users.noreply.github.com>
2023-04-10 11:45:55 +0900

    learned gameinstance chapter

---
+++ Source/Udemy/PuzzlePlatformsGameInstance.cpp
@@ -0,0 +1,11 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PuzzlePlatformsGameInstance.h"
+
+UPuzzlePlatformsGameInstance::UPuzzlePlatformsGameInstance(const FObjectInitializer& ObjectInitializer) {
+	UE_LOG(LogTemp, Warning, TEXT("Im »ý¼ºÀÚ"));
+}
+void UPuzzlePlatformsGameInstance::Init() {
+	UE_LOG(LogTemp, Warning, TEXT("Im Init"));
+}
